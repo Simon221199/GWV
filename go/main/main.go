183a031d85a46d3cab875ba8b0ccdceb6406cd49
patrickz98 <patrickz98@users.noreply.github.com>
2019-11-01 12:21:28 +0100

    Clean up

--- go/main/main.go
+++ go/main/main.go
@@ -25,8 +25,6 @@ func (cell cell) coordinates() string {
 	return fmt.Sprintf("(%d, %d)", cell.inx, cell.iny)
 }
 
-type grid [][]*cell
-
 func DistanceEuclidean(goal, node *cell) int {
 
 	tmp := math.Pow(float64(node.inx-goal.inx), 2) +
@@ -37,13 +35,20 @@ func DistanceEuclidean(goal, node *cell) int {
 	return -int(math.Round(h))
 }
 
-func findNeighbours(grid grid, node *cell) []*cell {
+type field struct {
+	cells       [][]*cell
+	coordinates map[string]*cell
+	start       *cell
+	goal        *cell
+}
+
+func (grid field) findNeighbours(node *cell) []*cell {
 
 	startX := simple.Max(0, node.inx-1)
-	endX := simple.Min(len(grid[ 0 ]), node.inx+1)
+	endX := simple.Min(len(grid.cells[ 0 ]), node.inx+1)
 
 	startY := simple.Max(0, node.iny-1)
-	endY := simple.Min(len(grid), node.iny+1)
+	endY := simple.Min(len(grid.cells), node.iny+1)
 
 	neighbours := make([]*cell, 0)
 
@@ -60,7 +65,7 @@ func findNeighbours(grid grid, node *cell) []*cell {
 				continue
 			}
 
-			cell := grid[ iny ][ inx ]
+			cell := grid.cells[ iny ][ inx ]
 
 			if ! cell.blocked {
 				neighbours = append(neighbours, cell)
@@ -71,70 +76,74 @@ func findNeighbours(grid grid, node *cell) []*cell {
 	return neighbours
 }
 
-func calculateDistances(grid grid, goal *cell) {
+func (grid *field) calculateDistances() {
 
-	for iny := range grid {
-		for inx := range grid[ iny ] {
+	for iny := range grid.cells {
+		for inx := range grid.cells[ iny ] {
 
-			cell := grid[ iny ][ inx ]
+			cell := grid.cells[ iny ][ inx ]
 			// cell.priority = DistanceManhattan(goal, cell)
-			cell.priority = DistanceEuclidean(goal, cell)
+			cell.priority = DistanceEuclidean(grid.goal, cell)
 		}
 	}
 }
 
-func printPriorities(grid grid) {
-	for iny := range grid {
-		for inx := range grid[ iny ] {
+func (grid field) priorityMatrix() string {
 
-			cell := grid[ iny ][ inx ]
+	matrix := ""
+
+	for iny := range grid.cells {
+		for inx := range grid.cells[ iny ] {
+
+			cell := grid.cells[ iny ][ inx ]
 
 			if cell.blocked {
-				fmt.Printf("%6s", "X")
+				matrix += fmt.Sprintf("%3s", "X")
 			} else {
-				fmt.Printf("%6d", cell.priority)
-			}
-			// fmt.Printf("(%d, %d) --> %d\n", cell.iny, cell.inx, cell.priority)
-		}
-
-		fmt.Println()
-	}
-}
-
-func createHashMap(grid grid) map[string]*cell {
-
-	hashMap := make(map[string]*cell)
-
-	for iny := range grid {
-		for inx := range grid[ iny ] {
-			cell := grid[ iny ][ inx ]
-
-			if ! cell.blocked {
-				hashMap[ cell.coordinates() ] = cell
+				matrix += fmt.Sprintf("%3d", cell.priority)
 			}
 		}
+
+		matrix += "\n"
 	}
 
-	return hashMap
+	return matrix
 }
 
-// func printPath(grid grid, path []*cell) {
-func printPath(grid grid, path stack.Stack) {
+// func (grid field) coordinatesMap() map[string]*cell {
+//
+// 	hashMap := make(map[string]*cell)
+//
+// 	for iny := range grid.cells {
+// 		for inx := range grid.cells[ iny ] {
+// 			cell := grid.cells[ iny ][ inx ]
+//
+// 			if ! cell.blocked {
+// 				hashMap[ cell.coordinates() ] = cell
+// 			}
+// 		}
+// 	}
+//
+// 	return hashMap
+// }
+
+// func printPath(field field, path []*cell) {
+func (grid field) printPath(path *stack.Stack) {
 
 	// for _, point := range path {
 	//
-	// 	tmp := grid[ point.iny ][ point.inx ].symbol + ""
-	// 	// grid[ point.iny ][ point.inx ].symbol = "*"
+	// 	tmp := field[ point.iny ][ point.inx ].symbol + ""
+	// 	// field[ point.iny ][ point.inx ].symbol = "*"
 	//
-	// 	for iny := range grid {
-	// 		for inx := range grid[ iny ] {
-	// 			fmt.Printf("%s", grid[ iny ][ inx ].symbol)
+	// 	for iny := range field {
+	// 		for inx := range field[ iny ] {
+	// 			fmt.Printf("%s", field[ iny ][ inx ].symbol)
 	// 		}
 	//
 	// 		fmt.Println()
 	// 	}
 	//
-	// 	grid[ point.iny ][ point.inx ].symbol = tmp
+	// 	field[ point.iny ][ point.inx ].symbol = tmp
 	// }
 
 	fmt.Printf("path length: %d\n", path.Len())
@@ -142,19 +151,85 @@ func printPath(grid grid, path stack.Stack) {
 	for path.Len() > 0 {
 
 		point := path.Pop().(*cell)
-		// tmp := grid[ point.iny ][ point.inx ].symbol + ""
-		grid[ point.iny ][ point.inx ].symbol = "*"
+		// tmp := field[ point.iny ][ point.inx ].symbol + ""
+		grid.cells[ point.iny ][ point.inx ].symbol = "*"
 
-		for iny := range grid {
-			for inx := range grid[ iny ] {
-				fmt.Printf("%s", grid[ iny ][ inx ].symbol)
+		for iny := range grid.cells {
+			for inx := range grid.cells[ iny ] {
+				fmt.Printf("%s", grid.cells[ iny ][ inx ].symbol)
 			}
 
 			fmt.Println()
 		}
 
-		// grid[ point.iny ][ point.inx ].symbol = tmp
+		// field[ point.iny ][ point.inx ].symbol = tmp
 	}
+}
+
+func Init(path string) (*field, error) {
+
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	env := strings.TrimSpace(string(bytes))
+	lines := strings.Split(env, "\n")
+
+	cells := make([][]*cell, len(lines))
+	coordinates := make(map[string]*cell)
+
+	var goal *cell
+	var start *cell
+
+	for iny, line := range lines {
+
+		fields := strings.Split(line, "")
+		cells[ iny ] = make([]*cell, len(fields))
+
+		for inx, str := range fields {
+
+			blocked := false
+			if str == "x" {
+				blocked = true
+			}
+
+			cell := &cell{
+				inx:     inx,
+				iny:     iny,
+				blocked: blocked,
+				symbol:  str,
+			}
+
+			cells[ iny ][ inx ] = cell
+			coordinates[ cell.coordinates() ] = cell
+
+			if str == "g" {
+				goal = cell
+			}
+
+			if str == "s" {
+				start = cell
+			}
+		}
+
+		fmt.Println(strings.Split(line, ""))
+	}
+
+	if start == nil || goal == nil {
+		return nil, fmt.Errorf("error: start == nil || goal == nil")
+	}
+
+	grid := &field{
+		cells: cells,
+		coordinates: coordinates,
+		start: start,
+		goal:  goal,
+	}
+
+	grid.calculateDistances()
+
+	return grid, nil
 }
 
 func main() {
@@ -165,77 +240,16 @@ func main() {
 		path = os.Args[ 1 ]
 	}
 
-	fmt.Printf("Sourcing %s\n", path)
-	bytes, err := ioutil.ReadFile(path)
+	fmt.Printf("sourcing %s\n", path)
+
+	grid, err := Init(path)
 	if err != nil {
 		panic(err)
 	}
 
-	env := strings.TrimSpace(string(bytes))
-	lines := strings.Split(env, "\n")
-
-	grid := make(grid, len(lines))
-	fmt.Printf("lines: %d\n", len(lines))
-
-	var goal *cell
-	var start *cell
-
-	for iny, line := range lines {
-
-		fields := strings.Split(line, "")
-		grid[ iny ] = make([]*cell, len(fields))
-
-		for inx, str := range fields {
-
-			blocked := false
-			if str == "x" {
-				blocked = true
-			}
-
-			grid[ iny ][ inx ] = &cell{
-				inx:     inx,
-				iny:     iny,
-				blocked: blocked,
-				symbol:  str,
-			}
-
-			// fmt.Println("str=" + str)
-
-			if str == "g" {
-				// fmt.Printf("goal %d, %d\n", iny, inx)
-				goal = grid[ iny ][ inx ]
-			}
-
-			if str == "s" {
-				// fmt.Printf("start %d, %d\n", iny, inx)
-				start = grid[ iny ][ inx ]
-			}
-		}
-
-		fmt.Println(strings.Split(line, ""))
-	}
-
-	if start == nil || goal == nil {
-		fmt.Println("Error: start == nil || goal == nil")
-		os.Exit(1)
-	}
-
-	calculateDistances(grid, goal)
-	printPriorities(grid)
-
-	cells := createHashMap(grid)
-
-	// neighbours := findNeighbours(grid, start)
-	// fmt.Println(start.coordinates())
-	// fmt.Println("#############################")
-	//
-	// for _, cell := range neighbours {
-	// 	fmt.Println(cell.coordinates())
-	// }
-
 	pq := make(queue.PriorityQueue, 0)
 	pq.Push(&queue.Item{
-		Value:    start.coordinates(),
+		Value:    grid.start.coordinates(),
 		Priority: 1,
 	})
 	heap.Init(&pq)
@@ -248,7 +262,7 @@ func main() {
 	for pq.Len() > 0 {
 
 		item := heap.Pop(&pq).(*queue.Item)
-		cell := cells[ item.Value ]
+		cell := grid.coordinates[ item.Value ]
 
 		fmt.Printf("cell (%d) >> %s\n", cell.priority, cell.coordinates())
 
@@ -256,14 +270,14 @@ func main() {
 		// path = append(path, cell)
 		pathCells.Push(cell)
 
-		if cell.coordinates() == goal.coordinates() {
+		if cell.coordinates() == grid.goal.coordinates() {
 			fmt.Println("Done")
 			break
 		}
 
-		neighbours := findNeighbours(grid, cell)
+		neighbours := grid.findNeighbours(cell)
 
-		xxx := pq.Len()
+		pop := true
 
 		for _, neighbour := range neighbours {
 
@@ -279,12 +293,14 @@ func main() {
 				Value:    neighbour.coordinates(),
 				Priority: neighbour.priority,
 			})
+
+			pop = false
 		}
 
-		if xxx == pq.Len() {
+		if pop {
 			pathCells.Pop()
 		}
 	}
 
-	printPath(grid, *pathCells)
+	grid.printPath(pathCells)
 }
